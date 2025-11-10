946c91a9dc07cb8196bd4ef06150fcd5d28e3bd1
joaof <joaof@NoteJR>
2025-11-10 20:39:42 -0300

    Feat: Adicionado Server RMI

---
+++ src/main/java/principal/RemoteHello.java
@@ -0,0 +1,12 @@
+
+package principal;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteHello extends Remote {
+
+    public String getMensagem() throws RemoteException;
+
+    public void setMensagem(String mensagem) throws RemoteException;
+}
